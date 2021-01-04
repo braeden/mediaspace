9234b2c3844b1c3da79092cf064d419e202e4889
braeden <braedensmith2@gmail.com>
2021-01-03 18:10:51 -0600

    add regex replace

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installation
+
+- Go to chrome://extensions/
+- Enable developer mode
+- Download and unzip this repository using the green button in the top right
+- Click "Load unpacked", select the folder you unzipped this repository into
+
+# Usage
+- Install ffmpeg on your computer ()
+- Open dev tools on the page with the mediaspace video
+- Open the console tab and click play on the video
+- There should be a console log with "ffmpeg ...."
+- Copy and paste those contents into a terminal and it should download your video
--- devtools.js
+++ devtools.js
@@ -2,7 +2,7 @@ chrome.devtools.network.onRequestFinished.addListener(function (req) {
     req.getContent((s) => {
         if (req.request.url.includes(".m3u8") && req.request.url.includes("jsonp")) {
             const o = JSON.parse(s.split('(')[1].split(')')[0])
-            chrome.devtools.inspectedWindow.eval(`console.warn('ffmpeg -i "${o.flavors[o.flavors.length-1].url}" -c copy', '"'+document.title.trim().replaceAll(':', '').replaceAll(' ', '-')+'.mp4"')`)
+            chrome.devtools.inspectedWindow.eval(`console.warn('ffmpeg -i "${o.flavors[o.flavors.length-1].url}" -c copy', '"'+document.title.trim().replace(/[^a-zA-Z0-9]/g,'_')+'.mp4"')`)
         }
     })
 });
